5f7a0b547c160b0cb93978bf6cbf0d3645258272
Bharath Kumar Gadiyaram <bgadiyaram@apple.com>
2021-10-31 08:23:22 -0700

    enable logging

--- src/main/java/com/interview/controller/v1/UserControllerV1.java
+++ src/main/java/com/interview/controller/v1/UserControllerV1.java
@@ -4,6 +4,8 @@ package com.interview.controller.v1;
 import com.interview.dto.v1.UserDetailsResponseV1;
 import com.interview.dto.v1.UserListResponseV1;
 import com.interview.service.v1.UserServiceV1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,16 +21,20 @@ import java.io.IOException;
  */
 public class UserControllerV1 {
 
+    public static Logger logger = LoggerFactory.getLogger(UserControllerV1.class);
+
     @Autowired
     UserServiceV1 userService;
 
     @GetMapping("/v1/users/list")
     public UserListResponseV1 getUsers(@RequestParam(value = "page")int page) throws IOException {
+        logger.info("requesting users for page: " + page);
         return userService.listUsers(page);
     }
 
     @GetMapping("/v1/user/{id}")
     public UserDetailsResponseV1 getUserDetails(@PathVariable(value = "id")String id) throws IOException {
+        logger.info("requesting users details for: " + id);
         return userService.getUserDetails(id);
     }
 }
